b00fad7640072bd04544f500d7252ee206c6b241
SangJun-HitHub <sjpark24@myseneca.ca>
2018-08-04 01:48:39 -0400

    commit

---
+++ Workshop1/at_home/tools.h
@@ -0,0 +1,12 @@
+#ifndef SITC_TOOLS_H
+#define SITC_TOOLS_H
+
+namespace sict
+{
+   // Displays the user interface menu
+   int menu();
+
+   // Performs a fool-proof integer entry
+   int getInt(int min, int max);
+}
+#endif
---
+++ Workshop1/in_lab/tools.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+// Displays the user interface menu
+int menu();
+
+// Performs a fool-proof integer entry
+int getInt(int min, int max);
